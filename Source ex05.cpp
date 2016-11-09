07d3d78406942ab6f69dc3826998614cd45719a4
marnierimes <mrimes19@my.whitworth.edu>
2016-11-09 00:06:27 -0800

    Initial commit

---
+++ Source ex05.cpp
@@ -0,0 +1,27 @@
+//
+//Marnie Rimes
+//HW06 ex05
+//Liang 14.7: Math: The Complex class
+//
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Complex
+{
+public:
+	Complex(a, b);
+	Complex(a);
+	Complex;
+	double add() {};
+	double subtract() {};
+	double multiply() {};
+	double divide() {};
+	double abs() {};
+	string toString() {};
+};
+
+int main()
+{
+
+}
